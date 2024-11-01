10db3991f5cb72930d4ca51efc843a3915595f5f
AmoryBotha <abotha181@gmail.com>
2024-11-01 12:11:07 +0200

    allowed origin

--- backend/settings.py
+++ backend/settings.py
@@ -152,5 +152,5 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = ["https://gmadevappfrontend.onrender.com"]
 CORS_ALLOWS_CREDENTIALS = True
